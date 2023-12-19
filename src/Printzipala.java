12ef2e6d260ff942c654beee4a00e419f7cb219c
iamLudok <122362676+iamLudok@users.noreply.github.com>
2023-12-19 16:54:31 +0100

    Update and rename Printzipala.java to Printzipala.java

--- src/Printzipala.java
+++ src/Printzipala.java
@@ -57,7 +57,7 @@ public class Printzipala implements PropertyChangeListener
 	{
 		SwingUtilities.invokeLater(() -> 
 		{
-            loginFrame = new JFrame("Inicio de Sesión");
+            loginFrame = new JFrame("Inicio de SesiÃ³n");
             loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             loginFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
             loginFrame.setUndecorated(true);
@@ -85,12 +85,12 @@ public class Printzipala implements PropertyChangeListener
             loginPanel.add(nombreField, gbc);
 
             gbc.gridy++;
-            loginPanel.add(new JLabel("Contraseña:"), gbc);
+            loginPanel.add(new JLabel("ContraseÃ±a:"), gbc);
 
             gbc.gridy++;
             loginPanel.add(contrasenaField, gbc);
 
-            JButton loginButton = new JButton("Iniciar Sesión");
+            JButton loginButton = new JButton("Iniciar SesiÃ³n");
             loginButton.setActionCommand("SAIOAHASI");
             loginButton.addActionListener(kontrolatzailea);
             
@@ -346,7 +346,7 @@ public class Printzipala implements PropertyChangeListener
         } 
 		else
         {
-            JOptionPane.showMessageDialog(null, "Autenticación fallida. Inténtalo de nuevo.");
+            JOptionPane.showMessageDialog(null, "AutenticaciÃ³n fallida. IntÃ©ntalo de nuevo.");
         }
 	}
 	
