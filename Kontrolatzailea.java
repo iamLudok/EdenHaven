f3e5b6f25f0d3bda8a51f3028ea3a913b12d40bf
iamLudok <122362676+iamLudok@users.noreply.github.com>
2023-12-19 18:10:29 +0100

    Add files via upload

---
+++ Kontrolatzailea.java
@@ -0,0 +1,66 @@
+package proiektua_2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Kontrolatzailea implements ActionListener
+{
+	Printzipala irudia;
+	Modeloa modeloa;
+	int pos;
+	
+	public Kontrolatzailea(Modeloa modeloa) 
+	{
+		irudia = null;
+		this.modeloa = modeloa;
+		pos = 0;
+	}
+	
+	public void setIrudia (Printzipala irudia) 
+	{
+		this.irudia = irudia;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+	    if ("SAIOAHASI".equals(e.getActionCommand())) 
+	    {
+	    	irudia.autenticarUsuario();
+	    } 
+	    else if (("HUECO".equals(e.getActionCommand())) || (("HUECO2".equals(e.getActionCommand()))) || (("HUECO3".equals(e.getActionCommand()))))
+	    {
+	    	switch(e.getActionCommand()) 
+	    	{
+		    	case "HUECO":
+		    	{
+		    		pos = 1;
+		    		break;
+		    	}
+		    	case "HUECO2":
+		    	{
+		    		pos = 2;
+		    		break;
+		    	}
+		    	case "HUECO3":
+		    	{
+		    		pos = 3;
+		    		break;
+		    	}
+	    	}
+	    	irudia.mostrarOpciones();
+	    } 
+	    else if ("POZO".equals(e.getActionCommand())) 
+	    {
+	    	modeloa.bota("pozo", pos);
+	    } 
+	    else if ("VENTILADOR".equals(e.getActionCommand())) 
+	    {
+	    	modeloa.bota("ventilador", pos);
+	    } 
+	    else if ("ESTUFA".equals(e.getActionCommand())) 
+	    {
+	    	modeloa.bota("estufa", pos);
+	    }
+	}
+}
